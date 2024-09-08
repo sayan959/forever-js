df988b7d311eb4a1cc05394bf92eaef86729eced
Sayan Biswas <shayonbiswas111@gmail.com>
2024-09-09 02:55:02 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,202 @@
+
+// Button Clickde and After that go to the Product page
+function navigateToAnotherPage (page){
+    window.location.href = page;
+}
+
+//-----------Product quntity and Price calclutaion---------
+document.getElementById('buynow-button1').addEventListener('click', function(){
+    //---------Product quntity----------
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-1');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button2').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-2');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button3').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-3');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button4').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-4');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button5').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-5');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button6').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-6');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button7').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-7');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+document.getElementById('buynow-button8').addEventListener('click', function(){
+    const preProductQuntity = document.getElementById('products-quantity');
+    const preProductQuntityText = preProductQuntity.innerText;
+    const preProductQuntityNum  = parseInt(preProductQuntityText );
+    const finalProductQuntityNum = preProductQuntityNum +1 ;
+    preProductQuntity.innerText = finalProductQuntityNum ;
+
+    //---------All Product price----------
+    const preAllProductPrice = document.getElementById('all-products-price');
+    const preAllProductPriceText = preAllProductPrice.innerText;
+    const preAllProductPriceNum = parseFloat(preAllProductPriceText);
+    
+    //----------Product Price---------
+    const preProductPrice = document.getElementById('product-price-8');
+    const preProductPriceText = preProductPrice.innerText;
+    const preProductPriceNum = parseFloat(preProductPriceText);
+    const finalProductPrice = preAllProductPriceNum + preProductPriceNum;
+    preAllProductPrice.innerText = finalProductPrice;
+    
+    
+
+    
+})
+
+
+
+
+
